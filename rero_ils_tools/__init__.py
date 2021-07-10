459f2bf0fcddfe534df89150a08df384e618b3c9
Aly Badr <aly.badr@rero.ch>
2021-07-10 14:20:52 +0200

    loans: validate virtua checkout loans
    Co-Authored-by: Aly Badr <aly.badr@rero.ch>

--- rero_ils_tools/__init__.py
+++ rero_ils_tools/__init__.py
@@ -27,6 +27,7 @@ from .cli.items.replace import items_replace
 from .cli.items.update import items_update
 from .cli.patrons.duplicate_emails import duplicate_emails
 from .cli.patrons.fix_patron_emails import fix_patron_emails
+from .cli.patrons.validate_checkouts import validate_checkouts
 from .cli.query.query import records_query
 from .cli.update.circ_category import set_circulation_category
 
@@ -71,3 +72,4 @@ replace.add_command(items_replace)
 search.add_command(records_query)
 patrons.add_command(duplicate_emails)
 patrons.add_command(fix_patron_emails)
+patrons.add_command(validate_checkouts)
