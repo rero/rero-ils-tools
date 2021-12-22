1e3cf2c5412930f3b17581d86bf9b98edccd274a
Aly Badr <aly.badr@rero.ch>
2021-12-22 13:27:21 +0100

    desherbage: deleting items for the vs organisation
    A new cli to allow libraries of the vs organisation
    to delete items and update document local fields
    accordingly, in addition to listing serial holdings
    and modified objects.
    
    Co-Authored-by: Aly Badr <aly.badr@rero.ch>

---
+++ rero_ils_tools/cli/desherbage/vs.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# RERO ILS
+# Copyright (C) 2021 RERO
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""RERO ILS bibliomedia desherbage VS command line interface."""
+
+from __future__ import absolute_import, print_function
+
+import json
+import os
+import sys
+from datetime import datetime
+
+import click
+from elasticsearch_dsl import Q
+from flask.cli import with_appcontext
+from rero_ils.modules.api import IlsRecordError
+from rero_ils.modules.documents.api import Document
+from rero_ils.modules.documents.utils import title_format_text_head
+from rero_ils.modules.holdings.api import Holding
+from rero_ils.modules.items.api import Item, ItemsSearch
+from rero_ils.modules.libraries.api import Library
+from rero_ils.modules.local_fields.api import LocalField, LocalFieldsSearch
+from rero_ils.modules.utils import JsonWriter
+
+
+def validate_inputs(library_pid, save):
+    """Validate correct inputs are given."""
+    library = Library.get_record_by_pid(library_pid)
+    if not library:
+        click.secho(f'error: library record not found.', fg='red')
+        sys.exit()
+    if not os.path.exists(save):
+        click.secho(f'error: directory {save} does not exist.', fg='red')
+        sys.exit()
+    return library
+
+
+def write_to_log_file(msg, info):
+    """Write information into log file."""
+    click.echo(msg)
+    info.write(msg + '\n')
+
+
+def get_document_local_fields(document_pid, org_pid):
+    """."""
+    query_filters = [
+        Q('term', parent__type='doc'),
+        Q('term', parent__pid=document_pid),
+        Q('term', organisation__pid=org_pid),
+    ]
+    query = LocalFieldsSearch()\
+        .query('bool', filter=query_filters)\
+        .source(['pid'])
+
+    local_fields = []
+    for hit in query.scan():
+        local_field_pid = hit.pid
+        local_field = LocalField.get_record_by_pid(local_field_pid)
+        local_fields.append(local_field)
+    return local_fields
+
+
+def update_local_fields(
+        local_fields, library_code, document, docs_file,
+        local_fields_list, document_pid):
+    """Delete library code from local fields."""
+    for record in local_fields:
+        fields = list(record.get('fields', {}).keys())
+        for field in fields:
+            text_1 = f'$a {library_code}'
+            text_2 = f'$2 cdu-{library_code}'
+            data_field = ' '.join([
+                str(elem) for elem in record['fields'][field]])
+            if text_1 in data_field or text_2 in data_field:
+                docs_file.write(document)
+                msg = f"{document_pid}: {record.pid}: {record['fields'][field]}"
+                local_fields_list.write(msg + '\n')
+                del record['fields'][field]
+        if len(record.get('fields', {}).keys()):
+            record.update(record, dbcommit=True, reindex=True)
+        else:
+            record.delete(record, dbcommit=True, delindex=True)
+
+
+def number_of_items(library_pid, document_pid):
+    """Get number of items for given library."""
+    query_filters = [
+        Q('term', document__pid=document_pid),
+        Q('term', library__pid=library_pid)
+    ]
+    query = ItemsSearch()\
+        .query('bool', filter=query_filters)\
+        .source(['pid'])
+    return query.count()
+    
+    
+def manage_documents(
+        library_pid, document_pids, info, docs_file, docs_list, org_pid,
+        library_code, local_fields_list):
+    """Update document if needed."""
+    for document_pid in document_pids:
+        document = Document.get_record_by_pid(document_pid)
+        if document.get('partOf'):
+            sort_title = title_format_text_head(
+                document.get('title', []),
+                with_subtitle=True
+            )
+            msg = f'{document.pid}: {sort_title}'
+            docs_list.write(msg + '\n')
+        if not number_of_items(library_pid, document_pid):
+            local_fields = get_document_local_fields(document_pid, org_pid)
+            update_local_fields(
+                local_fields, library_code, document, docs_file,
+                local_fields_list, document_pid)
+
+
+def manage_holdings(holding_pids, info, holdings_list):
+    """List of serial holdings."""
+    for holding_pid in holding_pids:
+        holding = Holding.get_record_by_pid(holding_pid)
+        if holding and holding.holdings_type == 'serial':
+            msg = f'{holding.pid}'
+            holdings_list.write(msg + '\n')
+
+
+@click.command('vs')
+@click.argument('infile', type=click.File('r'))
+@click.option('-l', '--library_pid', required=True, help='Library PID.')
+@click.option('-c', '--library_code', required=True, help='Library code.')
+@click.option('-s', '--save', required=True, help='Directory to saving files.')
+@click.option('-v', '--verbose', is_flag=True, default=False,help='Verbose.')
+@with_appcontext
+def vs(
+        infile, library_pid, library_code, save, verbose):
+    """Delete library items.
+
+    infile: Text file contains the item barcodes to delete.
+    :param library_pid: The PID of the library.
+    :param library_code: The code of the library.
+    :param save: The directory where to save output files.
+    """
+    library = validate_inputs(library_pid, save)
+    click.secho(f'Delete items for library: {library.get("name")}', fg='red')
+
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    docs_file = JsonWriter(
+        os.path.join(save, f'documents_{timestamp}.json'))
+    items_file = JsonWriter(os.path.join(save, f'items_{timestamp}.json'))
+    docs_list = open(
+        os.path.join(save, f'documents_part_of_{timestamp}.txt'), 'w')
+    holdings_list = open(
+        os.path.join(save, f'holding_serials_{timestamp}.txt'), 'w')
+    info = open(os.path.join(save, f'vs_log_{timestamp}.log'), 'w')
+    local_fields_list = open(
+        os.path.join(save, f'local_fields_{timestamp}.txt'), 'w')
+
+    org_pid = library.organisation_pid
+    barcodes = infile.readlines()
+    holding_pids = []
+    document_pids = []
+    items_not_in_db = 0
+    items_not_deleted = 0
+    items_deleted = 0
+    for idx, line in enumerate(barcodes, 1):
+        barcode = line.rstrip()
+        item = Item.get_item_by_barcode(barcode, org_pid)
+        if not item:
+            msg = (f'Item barcode: "{barcode}" does not exist in database.')
+            write_to_log_file(msg, info)
+            items_not_in_db +=1
+        elif item.reasons_not_to_delete():
+            reasons = item.reasons_not_to_delete()
+            can, reasons = item.can_delete
+            text = 'can not be deleted. reasons:'
+            msg = (f'Item barcode: "{barcode}" {text} {json.dumps(reasons)}')
+            write_to_log_file(msg, info)
+            items_not_deleted += 1
+        else:
+            document_pid = item.document_pid
+            holding_pid = item.holding_pid
+            try:
+                items_file.write(item)
+                item.delete(item, dbcommit=True, delindex=True)
+                items_deleted += 1
+                msg = (f'Item barcode: "{barcode}" deleted from database.')
+                write_to_log_file(msg, info)
+                holding_pids.append(holding_pid)
+                document_pids.append(document_pid)
+            except IlsRecordError.NotDeleted:
+                msg = (f'Item barcode: "{barcode}" unable to delete.')
+                write_to_log_file(msg, info)
+                items_not_deleted += 1
+    
+    manage_holdings(list(set(holding_pids)), info, holdings_list)
+    manage_documents(
+        library_pid, list(set(document_pids)), info, docs_file, docs_list,
+        org_pid, library_code, local_fields_list)
+    count = f'Count: {idx}'
+    deleted = f', Deleted: {items_deleted}'
+    not_in_db = f', Not in DB: {items_not_in_db}'
+    not_deleted = f', Not deleted: {items_not_deleted}'
+    msg = f'{count}{deleted}{not_in_db}{not_deleted}'
+    click.secho(msg, fg='green')
+    assert idx == items_deleted + items_not_in_db + items_not_deleted
